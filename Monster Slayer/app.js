2b8a649b64374a6c5aa32bc8284b2db4189baf9e
Steven <stevenhuynh17@gmail.com>
2018-05-11 16:04:24 -0700

    feat: alerts user whether they won or lost

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -17,10 +17,22 @@ new Vue({
       var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
       this.monsterHealth -= damage;
 
+      if(this.monsterHealth <= 0) {
+        alert('YOU WON!');
+        this.gameIsRunning = false;
+        return;
+      }
+
       max = 12;
       min = 5;
       damage = Math.max(Math.floor(Math.random() * max) + 1, min)
       this.playerHealth -= damage;
+
+      if(this.playerHealth <= 0) {
+        alert('YOU LOST!');
+        this.gameIsRunning = false;
+        return;
+      }
     },
     specialAttack: function() {
 
