0fa26e68a4a06528ae0dd35490c472a21e984af2
Steven <stevenhuynh17@gmail.com>
2018-05-11 16:02:03 -0700

    feat: healthbar is being dynamic and functioning

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -10,6 +10,26 @@ new Vue({
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
+    },
+    attack: function() {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.monsterHealth -= damage;
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.playerHealth -= damage;
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
 });
